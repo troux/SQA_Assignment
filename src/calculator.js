2cfe04bbe88ff23e2c9c5964697768ddce8878f8
nano <thomas.roux@epitech.eu>
2019-04-12 15:43:55 +0100

    square function done, ready to merge on master

--- src/calculator.js
+++ src/calculator.js
@@ -1,33 +1,41 @@
 exports.add = function (firstNumber, secondNumber) {
     if (isNaN(firstNumber) || isNaN(secondNumber)) {
-      return "Error, both input must be number";
+        return "Error, both input must be number";
     } else {
-      return firstNumber + secondNumber;
+        return firstNumber + secondNumber;
     }
-  };
+};
 
 exports.substract = function (firstNumber, secondNumber) {
     if (isNaN(firstNumber) || isNaN(secondNumber)) {
-      return "Error, both input must be number";
+        return "Error, both input must be number";
     } else {
-      return firstNumber - secondNumber;
+        return firstNumber - secondNumber;
     }
-  };
+};
 
 exports.multiply = function (firstNumber, secondNumber) {
     if (isNaN(firstNumber) || isNaN(secondNumber)) {
-      return "Error, both input must be number";
+        return "Error, both input must be number";
     } else {
-      return firstNumber * secondNumber;
+        return firstNumber * secondNumber;
     }
-  };
+};
 
 exports.divide = function (firstNumber, secondNumber) {
     if (isNaN(firstNumber) || isNaN(secondNumber)) {
-      return "Error, both input must be number";
+        return "Error, both input must be number";
     } else if (secondNumber === 0) {
-      return "Error, division by zero"
+        return "Error, division by zero"
     } else {
-      return firstNumber / secondNumber;
+        return firstNumber / secondNumber;
     }
-  };
+};
+
+exports.square = function (number) {
+    if (isNaN(number)) {
+        return "Error, input must be number";
+    } else {
+        return number * number;
+    }
+};
--- test/calculatorTest.js
+++ test/calculatorTest.js
@@ -1,60 +1,72 @@
-
 let calculator = require('../src/calculator');
 let expect = require('chai').expect;
 
 describe('add', () => {
-  it('should add 2 number and return the result', () => {
-    const result = calculator.add(4,4);
-    expect(result).to.be.a('number');
-    expect(result).to.be.equal(8);
-  });
-  it('should return an error because of wrong input type', () => {
-    const result = calculator.add("2", "w");
-    expect(result).to.be.a('string');
-    expect(result).to.be.equal("Error, both input must be number")
-  })
+    it('should add 2 number and return the result', () => {
+        const result = calculator.add(4, 4);
+        expect(result).to.be.a('number');
+        expect(result).to.be.equal(8);
+    });
+    it('should return an error because of wrong input type', () => {
+        const result = calculator.add("2", "w");
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, both input must be number")
+    })
 });
 
 describe('substract', () => {
-  it('should add 2 number and return the result', () => {
-    const result = calculator.substract(6,2);
-    expect(result).to.be.a('number');
-    expect(result).to.be.equal(4);
-  });
-  it('should return an error because of wrong input type', () => {
-    const result = calculator.substract("sajdb", "awoinweo");
-    expect(result).to.be.a('string');
-    expect(result).to.be.equal("Error, both input must be number")
-  })
+    it('should add 2 number and return the result', () => {
+        const result = calculator.substract(6, 2);
+        expect(result).to.be.a('number');
+        expect(result).to.be.equal(4);
+    });
+    it('should return an error because of wrong input type', () => {
+        const result = calculator.substract("sajdb", "awoinweo");
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, both input must be number")
+    })
 });
 
 describe('mutiply', () => {
-  it('should add 2 number and return the result', () => {
-    const result = calculator.multiply(3,5);
-    expect(result).to.be.a('number');
-    expect(result).to.be.equal(15);
-  });
-  it('should return an error because of wrong input type', () => {
-    const result = calculator.multiply("niwvw", "vsaisd");
-    expect(result).to.be.a('string');
-    expect(result).to.be.equal("Error, both input must be number")
-  })
+    it('should add 2 number and return the result', () => {
+        const result = calculator.multiply(3, 5);
+        expect(result).to.be.a('number');
+        expect(result).to.be.equal(15);
+    });
+    it('should return an error because of wrong input type', () => {
+        const result = calculator.multiply("niwvw", "vsaisd");
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, both input must be number")
+    })
 });
 
 describe('divide', () => {
-  it('should add 2 number and return the result', () => {
-    const result = calculator.divide(20,5);
-    expect(result).to.be.a('number');
-    expect(result).to.be.equal(4);
-  });
-  it('should return an error because of wrong input type', () => {
-    const result = calculator.divide("waeg", "wweaovi");
-    expect(result).to.be.a('string');
-    expect(result).to.be.equal("Error, both input must be number")
-  })
-  it('should return an error because of division by zero', () => {
-    const result = calculator.divide(1, 0);
-    expect(result).to.be.a('string');
-    expect(result).to.be.equal("Error, division by zero")
-  })
+    it('should add 2 number and return the result', () => {
+        const result = calculator.divide(20, 5);
+        expect(result).to.be.a('number');
+        expect(result).to.be.equal(4);
+    });
+    it('should return an error because of wrong input type', () => {
+        const result = calculator.divide("waeg", "wweaovi");
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, both input must be number")
+    })
+    it('should return an error because of division by zero', () => {
+        const result = calculator.divide(1, 0);
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, division by zero")
+    })
+});
+
+describe('square', () => {
+    it('should add 2 number and return the result', () => {
+        const result = calculator.square(3);
+        expect(result).to.be.a('number');
+        expect(result).to.be.equal(9);
+    });
+    it('should return an error because of wrong input type', () => {
+        const result = calculator.square("niwvw");
+        expect(result).to.be.a('string');
+        expect(result).to.be.equal("Error, input must be number")
+    })
 });
